d8aaa5cd7535fc39551ed0cdcaae1f0753eb4225
darkobas <darkobas@gmail.com>
2018-08-07 13:00:17 +0200

    libcameraservice: Add TARGET_CAMERA_NEEDS_CLIENT_INFO
    Change-Id: I85c2e9d4ffb56cc08aea1c1eabc1ed2ec45c0217
    
    SKULSHADY: Convert to soong config variable
    
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Change-Id: Id4fd49164c9a4f093e7decbd446a03aaf5369507

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -24,6 +24,10 @@
 #include <cstring>
 #include <ctime>
 #include <string>
+#ifdef CAMERA_NEEDS_CLIENT_INFO
+#include <iostream>
+#include <fstream>
+#endif
 #include <sys/types.h>
 #include <inttypes.h>
 #include <pthread.h>
@@ -2991,6 +2995,11 @@ status_t CameraService::BasicClient::startCameraOps() {
     // Notify listeners of camera open/close status
     sCameraService->updateOpenCloseStatus(mCameraIdStr, true/*open*/, mClientPackageName);
 
+#ifdef CAMERA_NEEDS_CLIENT_INFO
+    std::ofstream cpf("/data/misc/havoc/client_package_name");
+    std::string cpn = String8(mClientPackageName).string();
+    cpf << cpn;
+#endif
     return OK;
 }
 
